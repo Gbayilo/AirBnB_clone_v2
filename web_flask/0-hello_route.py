a4c39d907e9845e32ebd5483a3b407748193171d
Oluwagbayilo Odetunde <princegeeby1@gmail.com>
2024-07-28 20:57:29 +0100

    Adjust solution

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """Starts a Flask web application.
 
 The application listens on 0.0.0.0, port 5000.
@@ -9,7 +10,7 @@ Routes:
 
 from flask import Flask
 
-app = Flask("__name__")
+app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
